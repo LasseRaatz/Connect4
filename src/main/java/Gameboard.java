40aa744c62a8560ba3026c701874aac616f05d8c
Lasse Raatz <lraa@itu.dk>
2018-06-29 22:07:26 +0200

    Added getters for number of rows and columns to Gameboard and reworked GameboardTest to fit the changes

--- src/main/java/Gameboard.java
+++ src/main/java/Gameboard.java
@@ -13,7 +13,7 @@ public interface Gameboard {
     /**
      * Get the player number of the peg in the given position of the given column.
      * @param column the column number of the given column.
-     * @param position the position of the peg in the column. 1 is bottom.
+     * @param position the position of the peg in the column. 0 is bottom.
      * @return the player number of the owner of the peg.
      */
     int getOwnerOfPeg(int column, int position);
@@ -24,4 +24,14 @@ public interface Gameboard {
      * @return the number of pegs in the given column.
      */
     int getColumnSize(int column);
+
+    /**
+     * @return the number of columns in the gameboard.
+     */
+    int getNumberOfColumns();
+
+    /**
+     * @return the number of rows in the gameboard.
+     */
+    int getNumberOfRows();
 }
--- src/test/java/GameboardTest.java
+++ src/test/java/GameboardTest.java
@@ -12,23 +12,27 @@ class GameboardTest {
 
     @Test
     void putPegAndGetOwnerOfPeg() {
-        board.putPeg(1,1);
-        board.putPeg(2,1);
-        board.putPeg(1,1);
-        assertEquals(1, board.getOwnerOfPeg(1,1));
-        assertEquals(2, board.getOwnerOfPeg(1,2));
-        assertEquals(1, board.getOwnerOfPeg(1,3));
+        if (board.getNumberOfRows() < 3) throw new IllegalArgumentException("The given Gameboard does not have enough rows to be tested");
+        board.putPeg(1,board.getNumberOfColumns()-1);
+        board.putPeg(2,board.getNumberOfColumns()-1);
+        board.putPeg(1,board.getNumberOfColumns()-1);
+        assertEquals(1, board.getOwnerOfPeg(board.getNumberOfColumns()-1,0));
+        assertEquals(2, board.getOwnerOfPeg(board.getNumberOfColumns()-1,1));
+        assertEquals(1, board.getOwnerOfPeg(board.getNumberOfColumns()-1,2));
     }
 
     @Test
     void getColumnSize() {
-        for (int i = 0; i < 3; i++) board.putPeg(1,1);
-        for (int i = 0; i < 2; i++) board.putPeg(1,2);
-        for (int i = 0; i < 1; i++) board.putPeg(1,3);
-        assertEquals(3, board.getColumnSize(1));
-        assertEquals(2, board.getColumnSize(2));
-        assertEquals(1, board.getColumnSize(3));
-        assertEquals(0, board.getColumnSize(4));
+        for (int i = 0; i < board.getNumberOfColumns(); i++) {
+            for (int j = 0; j < board.getNumberOfRows() - i; j++) {
+                board.putPeg(1, i);
+            }
+        }
+        for (int i = 0; i < board.getNumberOfColumns(); i++) {
+            for (int j = 0; j < board.getNumberOfRows() - i; j++) {
+                assertEquals(board.getNumberOfRows() - i, board.getColumnSize(i));
+            }
+        }
     }
 
 }
